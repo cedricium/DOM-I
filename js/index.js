57637fef0e63ef60fcd19b378ea87903643d4b85
Cedricium <amaya.cedric@gmail.com>
2019-04-10 18:54:41 -0700

    Create selectors for DOM elements for later referencing

--- js/index.js
+++ js/index.js
@@ -40,3 +40,50 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Navigation
+const navItems = document.querySelectorAll('nav a')
+navItems.forEach((el, index) => {
+  el.textContent = siteContent.nav[`nav-item-${index + 1}`]
+})
+
+// Call-to-Action Section
+const ctaHeader = document.querySelector('.cta .cta-text h1')
+ctaHeader.textContent = siteContent.cta.h1
+
+const ctaButton = document.querySelector('.cta .cta-text button')
+ctaButton.textContent = siteContent.cta.button
+
+const ctaLogo = document.getElementById('cta-img')
+ctaLogo.setAttribute('src', siteContent.cta["img-src"])
+
+// Main Content Section
+const mainSectionTitles = document.querySelectorAll('.top-content h4')
+const mainSectionContent = document.querySelectorAll('.top-content p')
+
+const featuresTitle = mainSectionTitles[0]
+const featuresContent = mainSectionContent[0]
+
+const aboutTitle = mainSectionTitles[1]
+const aboutContent = mainSectionContent[1]
+
+const servicesTitle = mainSectionTitles[2]
+const servicesContent = mainSectionContent[2]
+
+const productTitle = mainSectionTitles[3]
+const productContent = mainSectionContent[3]
+
+const visionTitle = mainSectionTitles[4]
+const visionContent = mainSectionContent[4]
+
+const mainSectionMiddleImage = document.getElementById('middle-img')
+
+// Contact Section
+const contactTitle = document.querySelector('.contact h4')
+const contactInfoParagraphs = document.querySelectorAll('.contact p')
+const contactAddress = contactInfoParagraphs[0]
+const contactPhone = contactInfoParagraphs[1]
+const contactEmail = contactInfoParagraphs[2]
+
+// Footer
+const footerContent = document.querySelector('footer p')
